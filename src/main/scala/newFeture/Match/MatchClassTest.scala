c3efe30b8f52a557863aa20d985d5bcd30cbf31a
wangke <wangke3@xiaomi.com>
2015-12-28 11:48:35 +0800

    add scala lang
    Signed-off-by: wangke <wangke3@xiaomi.com>

---
+++ src/main/scala/newFeture/Match/MatchClassTest.scala
@@ -0,0 +1,62 @@
+package newFeture.Match
+
+
+/**
+ * Created by wangke on 15-12-28.
+ */
+object MatchClassTest {
+
+
+  def main(args: Array[String]) {
+
+
+    val teacher = new Teacher("wangke1")
+    val student = new Student("wangke2")
+    val student2 = new Student2("wangke3")
+
+    //会调用Array的apply方法构造Array,如果
+    val people = Array(student, teacher, student2)
+    for (p <- people) {
+      p match {
+        case Student() => println("got a student")
+        case Teacher(name) => println("got a teacher"+name)
+        case Student2(name) => println("got a student2:" + name)
+      }
+    }
+
+  }
+
+  class Persion
+
+
+  class Student(val name: String) extends Persion
+
+  //不使用case class 实现unapply也可实现自动提取功能,自动提取使用的是这个函数
+  object Student {
+    //在进行匹配时,会将match的参数persion传给unapply方法进行提取
+    //如果case后的参数没有任何返回值,那么只需要返回false或者true即可
+    def unapply(person: Persion) = {
+      println("math the persion name")
+      true
+    }
+
+  }
+
+
+  class Student2(val name: String) extends Persion
+
+  object Student2 {
+
+    //在进行匹配时返回结果会带给case后的函数作为参数进行使用
+    //可以返回Option[*],在运行时要么得到的是Some[*],要么得到的是NULL
+    def unapply(persion: Persion): Option[String] = {
+
+      //这个返回值充分展示了传入是wangke3,返回是wangke4的结果
+      Option("wnagke4")
+    }
+
+  }
+
+  case class Teacher(name: String) extends Persion
+
+}
